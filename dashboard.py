6c4f42cf8220fd878e562d25613286a45242dc7d
pujansomaiya <pujansomaiya@gmail.com>
2025-11-04 19:43:01 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("CoinDCX AI Mock Trading Dashboard")
+
+logs = pd.read_csv("trades.csv") if "trades.csv" else pd.DataFrame(columns=["Time","Action","Price","Profit"])
+st.dataframe(logs)
 
